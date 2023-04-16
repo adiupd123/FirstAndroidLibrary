27128a1c2e58b7c9ceca0fef8bb53fda0cd1895c
Aditya Upadhyay <adiupd123@gmail.com>
2023-04-16 19:32:10 +0530

    Created Library

--- app/src/main/java/com/adiupd123/firstandroidlibrary/MainActivity.kt
+++ app/src/main/java/com/adiupd123/firstandroidlibrary/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.adiupd123.firstandroidlibrary.ui.theme.FirstAndroidLibraryTheme
+import com.adiupd123.image_preview.ImagePreview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
